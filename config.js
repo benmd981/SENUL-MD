8be07915a88562775033e04fbc44c2b9eda148d1
benmd981 <vnimsara492@gmail.com>
2024-10-21 20:36:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "vDBFFSBb#26yQd6k3wn_ZLaTqMHXFfTsLl03iwnTubgm8kQrdyMg",
+SESSION_ID: process.env.SESSION_ID || "ZQ5X2BwI#MrsiC7blMWDRTStC8IzhWtRutLZE7_TzD2fawFAMmFg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://imgtr.ee/images/2024/09/11/3e6c73da5849ff8b9e48781ab8acbfe4.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "👋 Hello *${pushname}*\n*I AM SENUL-MD I AM ALIVE NOW !*",
-SUDO_NB: process.env.SUDO_NB || "94762296665",
+SUDO_NB: process.env.SUDO_NB || "94762498519",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public"
 };
